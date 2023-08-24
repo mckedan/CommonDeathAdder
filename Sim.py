172b5121999813f195ff12e18d6f351234144c31
mckedan <danmckeon@yahoo.com>
2023-08-24 06:57:20 -0400

    Began troubleshooting Sim, found list/array issue

--- Sim.py
+++ Sim.py
@@ -3,11 +3,12 @@ import math
 
 sBot = SimBot(0,0,math.pi/2)
 
-steps = [0,0,0]
+steps = [0,0,0] #need to make this a list of a 3 index array, not a 3 index array
 
-for i in range(3):
+for i in range(3):#And have this iterate through the LIST
     #print(i)
-    steps[i] = sBot.move(2, i, 1)
+    steps[i] = sBot.move(2, 2, math.pi/2)
+    continue
 
 for i in steps:
     print(steps)
